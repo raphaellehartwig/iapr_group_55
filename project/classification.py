e0c87b4638fb459f2ab4e60187c6c57c19c9296f
lniederhauser <lucille.niederhauser@epfl.ch>
2024-05-30 01:41:53 +0200

    Rapport parties Lucille

--- project/classification.py
+++ project/classification.py
@@ -221,3 +221,105 @@ def generate_csv_file(subclasses, csv_path):
     df.reset_index(inplace=True)
     df.columns = columns
     df.to_csv(csv_path, index=False)
+
+
+def train_model(model, criterion, optimizer, scheduler, data_train, data_val, device, batch_size=4, num_epochs=25,
+                verbose=True, phases=['train', 'val']):
+    since = time.time()
+    train_losses = []
+    val_losses = []
+    train_accuracies = []
+    val_accuracies = []
+    train_f1 = []
+    val_f1 = []
+    preds = []
+    # Create a temporary directory to save training checkpoints
+    with TemporaryDirectory() as tempdir:
+        best_model_params_path = os.path.join(tempdir, 'best_model_params.pt')
+
+        torch.save(model.state_dict(), best_model_params_path)
+        best_f1 = 0
+        best_epoch = num_epochs
+
+        for epoch in range(num_epochs):
+            if verbose:
+                print(f'Epoch {epoch}/{num_epochs - 1}')
+                print('-' * 10)
+
+            # Each epoch has a training and validation phase
+            for phase in phases:
+                if phase == 'train':
+                    model.train()  # Set model to training mode
+                    dataloader = DataLoader(data_train, batch_size=batch_size, shuffle=True)
+                    data_length = len(data_train)
+                else:
+                    model.eval()   # Set model to evaluate mode
+                    dataloader = DataLoader(data_val, batch_size=batch_size)
+                    data_length = len(data_val)
+
+                running_loss = 0.0
+                running_f1 = 0.0
+                running_corrects = 0.0
+                # Iterate over data.
+                for labels, inputs in dataloader:
+                    inputs = inputs.to(device)
+                    labels = labels.to(device)
+
+                    # zero the parameter gradients
+                    optimizer.zero_grad()
+
+                    # forward
+                    # track history if only in train
+                    with torch.set_grad_enabled(phase == 'train'):
+                        outputs = model(inputs)
+                        loss = criterion(outputs, labels)
+                        preds_batch = torch.argmax(outputs, dim=1)
+
+                        # backward + optimize only if in training phase
+                        if phase == 'train':
+                            loss.backward()
+                            optimizer.step()
+                        # save validation predictions
+                        else:
+                            preds.append(((torch.argmax(labels, dim=1)), preds_batch))
+
+                    # statistics
+                    running_loss += loss.item() * inputs.size(0)
+                    running_corrects += torch.sum(preds_batch == torch.argmax(labels, dim=1))
+                    running_f1 += f1_score(preds_batch, torch.argmax(labels, dim=1), average='macro')
+
+                epoch_loss = running_loss/data_length
+                epoch_accuracy = running_corrects.double()/data_length
+                epoch_f1 = running_f1/len(dataloader)
+
+                if verbose:
+                    print(f'{phase} Loss: {epoch_loss:.4f}')
+                    print(f'{phase} Accuracy: {epoch_accuracy:.4f}')
+                    print(f'{phase} F1-score: {epoch_f1:.4f}')
+
+                if phase == 'train':
+                    scheduler.step()
+                    train_losses.append(epoch_loss)
+                    train_accuracies.append(epoch_accuracy)
+                    train_f1.append(epoch_f1)
+                else:
+                    val_losses.append(epoch_loss)
+                    val_accuracies.append(epoch_accuracy)
+                    val_f1.append(epoch_f1)
+
+                # deep copy the model
+                if phase == 'val' and epoch_f1 > best_f1:
+                    best_f1 = epoch_f1
+                    best_epoch = epoch
+                    torch.save(model.state_dict(), best_model_params_path)
+
+        time_elapsed = time.time() - since
+        print(f'Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
+        if 'val' in phases:
+            print(f'Best val F1_score: {best_f1:4f} at epoch: {best_epoch}')
+
+        # load best model weights
+        model.load_state_dict(torch.load(best_model_params_path))
+
+    return model, train_losses, val_losses, train_accuracies, val_accuracies, train_f1, val_f1, preds
+
--- project/utils.py
+++ project/utils.py
@@ -3,6 +3,7 @@ import numpy as np
 import torch
 from matplotlib import pyplot as plt
 from PIL import Image
+import cv2
 
 
 def downsample_images(original_path, save_path, size=(1500, 1000)):
@@ -30,20 +31,21 @@ def plot_metric(train_metrics, val_metrics, title, ylabel):
     plt.show()
 
 
-def compute_pred(outputs):
+"""def compute_pred(outputs):
     outputs = outputs.detach()
     outputs_rounded = torch.round(outputs)
     outputs_rounded[outputs_rounded < 0] = 0
 
-    return outputs_rounded
+    return outputs_rounded"""
 
 
-def compute_accuracy(outputs, labels):
+"""def compute_accuracy(outputs, labels):
     # preds = compute_pred(outputs)
     outputs = outputs.detach()
     n_correct = (np.array(outputs) == np.array(labels.tolist())).sum()
     accuracy = n_correct/labels.numel()
-    return accuracy
+    return accuracy"""
+
 
 def compute_f1(outputs, labels):
     preds = np.array(outputs)
@@ -65,3 +67,37 @@ def convert_np_to_pil(image_array):
         pil_img = Image.fromarray((img * 255).astype(np.uint8))  # Assuming the input numpy array has values in [0, 1]
         pil_images.append(pil_img)
     return pil_images
+
+def calculate_circle_properties(circles, images):
+    if circles is None:
+        return []
+
+    properties = []
+
+    for i, (x, y, r) in enumerate(circles):
+        perimeter = 2 * np.pi * r
+        area = np.pi * r ** 2
+
+        # Calculate the center of the image
+        height, width = images[i].shape[:2]
+        center_x, center_y = width // 2, height // 2
+
+        # Calculate the coordinates for the 50x50 square
+        top_left_x = max(center_x - 25, 0)
+        top_left_y = max(center_y - 25, 0)
+        bottom_right_x = min(center_x + 25, width)
+        bottom_right_y = min(center_y + 25, height)
+
+        # Extract the 50x50 region from the middle of the image
+        square = images[i][top_left_y:bottom_right_y, top_left_x:bottom_right_x]
+
+        hsv = cv2.cvtColor(square, cv2.COLOR_BGR2HSV)
+        rgb = cv2.cvtColor(square, cv2.COLOR_BGR2RGB)
+        data_h, data_s, data_v = cv2.split(hsv)
+        data_r, data_g, data_b = cv2.split(rgb)
+
+        properties.append((perimeter, area, data_h.mean(), data_s.mean(), data_v.mean(),
+                           data_r.mean(), data_g.mean(), data_b.mean()))
+
+
+    return properties
